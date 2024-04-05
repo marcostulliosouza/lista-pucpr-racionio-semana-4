8e42c5d38701456af9612cf211b0e65411cc5471
Marcos Tullio <marcostullio.s@gmail.com>
2024-04-04 21:49:31 -0300

    ex7 concluido

---
+++ ex7.py
@@ -0,0 +1,17 @@
+# Exercício de fixação 7:Dado o vetor 
+# nums = [3, 11, 6, 32, 15, 22, 4, 10, 5], 
+# crie uma matriz 3x3 com os três maiores 
+# elementos na primeira linha, os três elementos 
+# intermediários na segunda linha e 
+# os elementos menores na terceira linha.
+
+nums = [3, 11, 6, 32, 15, 22, 4, 10, 5]
+matriz = []
+for i in range(3):
+    matriz.append([])
+    for j in range(3):
+        maior = max(nums)
+        matriz[i].append(maior)
+        nums.remove(maior)
+for linha in matriz:
+    print(linha)
