61c0956f76ab481d46678bfa93a47f62bad12062
Marcos Tullio <marcostullio.s@gmail.com>
2024-04-04 21:22:01 -0300

    ex6 concluido

---
+++ ex6.py
@@ -0,0 +1,9 @@
+# Listas – métodos e funções built in
+
+# Exercício de fixação 6: Dado o vetor 
+# nums = [3, 7, 2, 9, 5, 6], 
+# crie um programa que, em uma linha, 
+# calcule a média dos seus elementos.
+
+nums = [3, 7, 2, 9, 5, 6]
+print(f'Média: {sum(nums)/len(nums):.2f}')
